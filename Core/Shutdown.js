6e8ce74872007947293dd5ef781a3f8774af7a35
R4aVEY <techlorde0@gmail.com>
2026-07-27 19:14:31 +0300

    Implement shutdown function for process management
    Add shutdown function to handle process termination signals and errors.

--- Core/Shutdown.js
+++ Core/Shutdown.js
@@ -1,1 +1,74 @@
+function shutdown(sock){
 
+
+process.on(
+"SIGINT",
+async()=>{
+
+
+console.log(
+"\
+\nStopping R4VEY-MD..."
+);
+
+
+try{
+
+
+if(sock){
+
+await sock.logout();
+
+}
+
+
+process.exit(0);
+
+
+}
+
+catch(error){
+
+console.error(error);
+
+process.exit(1);
+
+}
+
+
+});
+
+
+process.on(
+"uncaughtException",
+(error)=>{
+
+
+console.error(
+"Critical Error:",
+error
+);
+
+
+});
+
+
+process.on(
+"unhandledRejection",
+(error)=>{
+
+
+console.error(
+"Unhandled Promise:",
+error
+);
+
+
+});
+
+
+}
+
+
+module.exports =
+shutdown;
